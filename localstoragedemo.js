e7a2eb5920aa5ce1932cdb46cfccd342d53b61c0
anilchinchawade <anilchinchawade1@gmail.com>
2025-02-20 12:20:26 +0530

    session storage added

--- localstoragedemo.js
+++ localstoragedemo.js
@@ -3,18 +3,18 @@ console.log("Local storage demo");
 let subjects = ["OOPJ","Python","FSD"];
 
 //Add key-value pair in local storage data
-localStorage.setItem("myname","Anil");
-localStorage.setItem("surname","Chinchawade");
-localStorage.setItem("contactNo",9922641108);
-localStorage.setItem("subjectstaught",JSON.stringify(subjects));
+// localStorage.setItem("myname","Anil");
+// localStorage.setItem("surname","Chinchawade");
+// localStorage.setItem("contactNo",9922641108);
+// localStorage.setItem("subjectstaught",JSON.stringify(subjects));
 
 //Retrieve local storage data
-let myname = localStorage.getItem("myname");
-let surname = localStorage.getItem("surname");
-let localitems = JSON.parse(localStorage.getItem("subjectstaught"));
+// let myname = localStorage.getItem("myname");
+// let surname = localStorage.getItem("surname");
+// let localitems = JSON.parse(localStorage.getItem("subjectstaught"));
 
 //Print data
-console.log("Name:", myname);
-console.log("Surname:", surname);
-console.log("Subjects Taught:", localitems);
+// console.log("Name:", myname);
+// console.log("Surname:", surname);
+// console.log("Subjects Taught:", localitems);
 
---
+++ sessionStorage.js
@@ -0,0 +1,12 @@
+console.log("Session Storage Demo");
+
+// sessionStorage.setItem("sessionmyname","Anil");
+// sessionStorage.setItem("sessionsurname","Chinchawade");
+// sessionStorage.setItem("sessioncontactNo",9922641108);
+// sessionStorage.setItem("subjectstaught",JSON.stringify(subjects));
+
+// let sessionmyname = sessionStorage.getItem("sessionmyname");
+
+
+
+// console.log("Name: ",sessionmyname);
